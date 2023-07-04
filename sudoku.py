c98b2914246ab370e95897827bf638fd0553d221
Szymon Gałecki <szymongalecki@gmail.com>
2023-07-04 19:51:59 +0200

    Working solution with OOP version

--- sudoku.py
+++ sudoku.py
@@ -11,6 +11,60 @@ puzzle = [
 ]
 
 
+class Sudoku:
+    def __init__(self, grid: list[list[int]]) -> None:
+        self.grid = grid
+        self.size = 9
+
+    def board(self):
+        print(self.grid)
+
+    def row(self, r: int) -> list[int]:
+        return self.grid[r]
+
+    def col(self, c: int) -> list[int]:
+        return [d for d in [self.grid[r][c] for r in range(self.size)]]
+
+    def block(self, r: int, c: int) -> list[int]:
+        b = []
+        for r in range((r // 3) * 3, ((r // 3) * 3) + 3):
+            for c in range((c // 3) * 3, ((c // 3) * 3) + 3):
+                b.append(self.grid[r][c])
+        return b
+
+    def possible(self, r: int, c: int) -> list[int]:
+        digits = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        return list(
+            digits - set(self.block(r, c)) - set(self.row(r)) - set(self.col(c))
+        )
+
+    def next(self) -> tuple[int, int] | None:
+        for r in range(self.size):
+            for c in range(self.size):
+                if self.grid[r][c] == 0:
+                    return (r, c)
+        return None
+
+    def solve(self):
+        if self.next():
+            r, c = self.next()
+        else:
+            return True
+
+        digits = self.possible(r, c)
+        for d in digits:
+            self.grid[r][c] = d
+            if self.solve():
+                return True
+            self.grid[r][c] = 0
+        return False
+
+
+s = Sudoku(puzzle)
+s.solve()
+s.board()
+
+
 def row(puzzle: list[list[int]], r: int) -> list[int]:
     return puzzle[r]
 
@@ -48,14 +102,16 @@ def solve(puzzle: list[list[int]]):
     if next(puzzle):
         r, c = next(puzzle)
     else:
-        print(puzzle)
-        return puzzle
+        return True
 
     digits = possible(puzzle, r, c)
     for d in digits:
         puzzle[r][c] = d
-        solve(puzzle)
+        if solve(puzzle):
+            return True
         puzzle[r][c] = 0
+    return False
 
 
 solve(puzzle)
+print(puzzle)
